438e8edb6db4977ab1611cd31f7dabe6ad626842
Aatif Syed <aatif@aatifsyed.uk>
2022-05-25 21:43:20 +0100

    works

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,85 @@
-use std::collections::HashSet;
-
 use anyhow::Context;
+use async_recursion::async_recursion;
 use futures::future::join_all;
-use petgraph::{graph::NodeIndex, visit::IntoNodeReferences, Directed, Graph};
+use petgraph::graph::DiGraph;
 use reqwest::Client;
-use tokio::sync::RwLock;
+use std::collections::{HashMap, HashSet};
+use tokio::sync::{Mutex, RwLock};
+use tracing::{info, instrument};
 use url::Url;
 
-async fn get_webpage(client: &Client, url: Url) -> anyhow::Result<WebPage> {
-    let text = client
+async fn get_webpage(client: &Client, url: &Url) -> anyhow::Result<String> {
+    client
         .get(url.clone())
         .send()
         .await
         .context("GET failed")?
         .text()
         .await
-        .context("Couldn't get text")?;
-
-    Ok(WebPage { text, url })
-}
-
-pub struct WebPage {
-    pub url: Url,
-    pub text: String,
+        .context("Couldn't get text")
 }
 
 pub async fn build_graph(
     root: Url,
-    get_children: impl FnOnce(&str) -> Option<Vec<Url>>,
-) -> anyhow::Result<Graph<WebPage, (), Directed>> {
-    let client = Client::default();
-    let mut graph = Graph::new();
-
-    let root = get_webpage(&client, root)
-        .await
-        .context("Couldn't fetch root page")?;
-    let children = get_children(&root.text).unwrap_or_default();
-
-    let root_index = graph.add_node(root);
-    Ok(graph)
-}
-
-async fn populate(
-    client: &Client,
-    parent: NodeIndex,
-    children: Vec<Url>,
-    graph: &RwLock<Graph<WebPage, ()>>,
-    get_children: impl FnOnce(&str) -> Option<Vec<Url>>,
-    already_processed: &RwLock<HashSet<Url>>,
-) {
-    for child in join_all(
-        children
-            .iter()
-            .map(|url| get_node_index(graph, client, url)),
-    )
-    .await
-    {
-        if let Ok(child) = child {
-            graph.write().await.add_edge(parent, child, ());
-        }
+    get_children: &impl Fn(&Url, &str) -> Option<HashSet<Url>>,
+) -> DiGraph<Url, ()> {
+    let nodes = Default::default();
+    let edges = Default::default();
+    let client = Client::new();
+    edit_graph(&client, root, get_children, &nodes, &edges).await;
+    let nodes = nodes.into_inner();
+    let edges = edges.into_inner();
+    let mut graph = DiGraph::new();
+    let mut indices = HashMap::new();
+    for (url, _content) in nodes {
+        indices.insert(url.clone(), graph.add_node(url));
     }
+    for (from, to) in edges {
+        graph.add_edge(indices[&from], indices[&to], ());
+    }
+    graph
 }
 
-async fn get_node_index(
-    graph: &RwLock<Graph<WebPage, ()>>,
+#[async_recursion(?Send)]
+#[instrument(skip_all, fields(parent))]
+async fn edit_graph(
     client: &Client,
-    url: &Url,
-) -> anyhow::Result<NodeIndex> {
-    match graph
-        .read()
+    parent: Url,
+    get_children: &impl Fn(&Url, &str) -> Option<HashSet<Url>>,
+    nodes: &RwLock<HashMap<Url, Result<String, String>>>,
+    edges: &Mutex<HashSet<(Url, Url)>>,
+) {
+    if nodes.read().await.contains_key(&parent) {
+        return;
+    }
+    let res = get_webpage(client, &parent)
         .await
-        .node_references()
-        .find_map(|(ix, web_page)| match web_page.url == *url {
-            true => Some(ix),
-            false => None,
-        }) {
-        Some(ix) => Ok(ix),
-        None => {
-            let web_page = get_webpage(client, url.clone()).await?;
-            Ok(graph.write().await.add_node(web_page))
+        .map_err(|e| e.to_string());
+    {
+        let mut write = nodes.write().await;
+        match write.contains_key(&parent) {
+            true => return,
+            false => {
+                info!("Add nodes from {parent}");
+                write.insert(parent.clone(), res.clone());
+                drop(write);
+
+                if let Ok(s) = res {
+                    if let Some(children) = get_children(&parent, &s) {
+                        info!("Disovered {} children", children.len());
+                        let mut write = edges.lock().await;
+                        for child in &children {
+                            let newly_added = write.insert((parent.clone(), child.clone()));
+                            assert!(newly_added, "logic error - created same edge twice");
+                        }
+                        drop(write);
+                        join_all(children.into_iter().map(|new_parent| {
+                            edit_graph(client, new_parent, get_children, nodes, edges)
+                        }))
+                        .await;
+                    }
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,47 @@
-fn main() {
-    let x = include_str!("../test-assets/enum.APIKeyLocation.html");
-    println!("{x}");
+use std::collections::HashSet;
+
+use clap::Parser;
+use soup::{NodeExt, QueryBuilderExt, Soup};
+use tracing::debug;
+use url::Url;
+
+#[derive(Parser)]
+struct Args {
+    url: Url,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .init();
+    let args = Args::parse();
+    let graph = what_the_struct::build_graph(args.url, &|url, body| {
+        let soup = Soup::new(body);
+        let doc = soup
+            .tag("pre")
+            .class("rust")
+            .class("struct")
+            .find()
+            .or_else(|| soup.tag("pre").class("rust").class("enum").find())?;
+        debug!("Found struct");
+        let linked = doc
+            .tag("a")
+            .attr_name("href")
+            .find_all()
+            .map(|anchor| {
+                let href = anchor.get("href").expect("Already filtered by href");
+                match href.parse::<Url>() {
+                    Ok(url) => Ok(url),
+                    Err(url::ParseError::RelativeUrlWithoutBase) => url.join(&href),
+                    Err(e) => Err(e),
+                }
+            })
+            .filter_map(Result::ok)
+            .collect::<HashSet<_>>();
+        Some(linked)
+    })
+    .await;
+    println!("{:?}", petgraph::dot::Dot::new(&graph));
+    Ok(())
 }
